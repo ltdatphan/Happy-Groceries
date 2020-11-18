88d76742374cfe9acf36ff7634f992ef3c15df4b
CesarEPerez <cesarperezargota@gmail.com>
2020-11-18 01:15:49 -0500

    Created products page card generation and table reading logic

--- backend/products.sql
+++ backend/products.sql
@@ -1,6 +1,6 @@
-DROP TABLE Product;
+DROP TABLE Products;
 
-CREATE TABLE Product (
+CREATE TABLE Products (
     id          int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     prod_name   varchar(30),
     prod_desc   varchar(4000),
@@ -11,8 +11,68 @@ CREATE TABLE Product (
     prod_url    varchar(500)
 );
 
-INSERT INTO Product (prod_name, prod_desc, price, category, source, organic, prod_url)
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
 VALUES ("Banana", "Rich with potassium and absolutely delicious!", 0.69, "fruit", "imported", false, "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg");
 
-INSERT INTO Product (prod_name, prod_desc, price, category, source, organic, prod_url)
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Strawberries", "Super yummy in my tummy, hurrah!", 1.46, "fruit", "local", false, "https://media.grocerygateway.com/medias/00628055798118-1-base-conversionFormat-1200-conversionFormat.jpg?context=bWFzdGVyfGltYWdlc3wzMDkxOXxpbWFnZS9qcGVnfGltYWdlcy9oMGEvaDEzLzg4NDE2NDQ1Mzk5MzQuanBnfDU3OWMwZTAxYjBhMDA2ZTdhOTcyOWE4Yzg5NTEwNDMxZWZkNjViYTAwYzdiNTIyYmFkMDc3YzIyNTNhMGY2OWY");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
+VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
+
+INSERT INTO Products (prod_name, prod_desc, price, category, source, organic, prod_url)
 VALUES ("Apple", "Rich with vitamin C and crunchy to eat!", 0.49, "fruit", "local", false, "https://cdn3.volusion.com/hckyx.hepad/v/vspfiles/photos/CONC-REDAPPLE-2.jpg?v-cache=1396136943");
--- public/products.php
+++ public/products.php
@@ -1,38 +1,76 @@
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
+    <?php
+    $assetPath = "../assets";
+    $templatePath = "../templates";
+    $title = "Hello Groceries";
+    $exampleVariable = "This is the PRODUCTS page";
+    ?>
+    <!-- Boostrap CDN-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- External CSS-->
+    <link rel="stylesheet" href="<?= $assetPath?>/css/style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title?></title>
+</head>
+<body>
+    <!--Navigation-->
+    <?php include "$templatePath/header.php"?>
+    <!--/Navigation-->
+
+    <!--Page content-->
+    <div class="container">
+        <h1>Products</h1>
         <?php
-        $assetPath = "../assets";
-        $templatePath = "../templates";
-        $title = "Hello Groceries";
-        $exampleVariable = "This is the PRODUCTS page";
+            $myfile = fopen("../../../mysqlLogin.txt", "r") or die("Unable to open file!");
+            $username = fgets($myfile);
+            $password = fgets($myfile);
+            fclose($myfile);
+            $conn = mysqli_connect("localhost", "cperezar", "ByRujNis", "cperezar") or die(mysql_error());
+
+            $sql = "SELECT * FROM Products ORDER BY id DESC";
+            $result = mysqli_query($conn, $sql);
+            $num_results = mysqli_num_rows($result);
+
+            if ($num_results <= 0) die("Unable to find product data!");
+
+            echo '<div class="row">';
+            while($row = mysqli_fetch_assoc($result)) {
+                $id = $row["id"];
+                $prod_name = $row["prod_name"];
+                $prod_desc = $row["prod_desc"];
+                $price = $row["price"];
+                $category = $row["category"];
+                $source = $row["source"];
+                $organic = $row["organic"];
+                $prod_url = $row["prod_url"];?>
+                <div class="col-sm">
+                    <div class="card products-card">
+                        <img class="card-img-top" src="<?= $prod_url?>" alt="Product image" style="width:100%">
+                        <div class="card-body">
+                            <h4 class="card-title"><?= $prod_name?></h4>
+                            <p class="card-text"><?= substr($prod_desc,0,45)."..."?></p>
+                            <p class="card-text"><?= "\$".sprintf("%.2f",$price)." CAD"?></p>
+                            <a href="#" class="btn btn-primary">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+            echo '</div>';
         ?>
-        <!-- Boostrap CDN-->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    </div>
+        
+    <!--/Page content-->
 
-        <!-- External CSS-->
-        <link rel="stylesheet" href="<?= $assetPath?>/css/style.css">
+    <!--Footer-->
+    <?php include "$templatePath/footer.php"?>
+    <!--/Footer-->
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?= $title?></title>
-    </head>
-    <body>
-        <!--Navigation-->
-        <?php include "$templatePath/header.php"?>
-        <!--/Navigation-->
-
-        <!--Page content-->
-        <div class="container">
-            <h1>Products</h1>
-            <p>This page is under development</p>
-        </div>
-        <!--/Page content-->
-
-        <!--Footer-->
-        <?php include "$templatePath/footer.php"?>
-        <!--/Footer-->
-    
-    </body>
+</body>
 </html>
