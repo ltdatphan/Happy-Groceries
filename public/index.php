8b66c95f4431a825b067d13e8c903f0f606fd95e
David <ltdatphan@gmail.com>
2020-11-19 19:35:23 -0500

    Implemented landing page

--- public/index.php
+++ public/index.php
@@ -5,14 +5,14 @@ $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['pag
 // Include and show the requested page
 
 //Uncomment me if you are hosting on Moons
-$myfile = fopen("../../../mysqlLogin.txt", "r") or die("Unable to open file!");
-$username = preg_replace("/\s+/", "", fgets($myfile, 4096));
-$password = preg_replace("/\s+/", "", fgets($myfile, 4096));
-fclose($myfile);
-$conn = mysqli_connect("localhost", $username, $password, $username) or die("Cannot connect to database!");
+// $myfile = fopen("../../../mysqlLogin.txt", "r") or die("Unable to open file!");
+// $username = preg_replace("/\s+/", "", fgets($myfile, 4096));
+// $password = preg_replace("/\s+/", "", fgets($myfile, 4096));
+// fclose($myfile);
+// $conn = mysqli_connect("localhost", $username, $password, $username) or die("Cannot connect to database!");
 
 //Connection to local database. Comment below out if you are hosting on Moons
-//$conn = mysqli_connect("localhost", 'hello', 'world', 'hello') or die("Cannot connect to database!");
+$conn = mysqli_connect("localhost", 'hello', 'world', 'hello') or die("Cannot connect to database!");
 
 $sql = "SELECT * FROM Products ORDER BY id DESC";
 
