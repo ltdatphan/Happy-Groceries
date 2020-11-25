b92189e7ac8f507f34ce4d7791548dcbf1738b22
Michael <michael.tandoc@ryerson.ca>
2020-11-24 19:38:40 -0500

    Made DB credentials change based on server name

--- public/index.php
+++ public/index.php
@@ -2,24 +2,36 @@
 session_start();
 include '../templates/header.php';
 
+switch ($_SERVER["SERVER_NAME"]) {
+    case 'webdev.scs.ryerson.ca':
+        $myfile = fopen("../../../mysqlLogin.txt", "r") or die("Unable to open file!");
+        $host = 'localhost';
+        $username = preg_replace("/\s+/", "", fgets($myfile, 4096));
+        $password = preg_replace("/\s+/", "", fgets($myfile, 4096));
+        $database = $username;
+        fclose($myfile);
+        break;
+    case 'happygrocery.infinityfreeapp.com':
+        $host = 'sql111.epizy.com';
+        $username = 'epiz_27230234';
+        $password = 'fqCZGUlAw1X7T';
+        $database = 'epiz_27230234_hello';
+        break;
+    case 'localhost':
+        $host = 'localhost';
+        $username = 'hello';
+        $password = 'world';
+        $database = 'hello';
+        break;
+}
 
-// Uncomment me if you are hosting on Moons
-$myfile = fopen("../../../mysqlLogin.txt", "r") or die("Unable to open file!");
-$username = preg_replace("/\s+/", "", fgets($myfile, 4096));
-$password = preg_replace("/\s+/", "", fgets($myfile, 4096));
-fclose($myfile);
-$conn = mysqli_connect("localhost", $username, $password, $username) or die("Cannot connect to database!");
-
-//Connection to local database. Comment below out if you are hosting on Moons
-//$conn = mysqli_connect("localhost", 'hello', 'world', 'hello') or die("Cannot connect to database!");
-
-//Connection to infinity free SQL
-//$conn = mysqli_connect("sql111.epizy.com", 'epiz_27230234', 'fqCZGUlAw1X7T', 'epiz_27230234_hello') or die("Cannot connect to database!");
+$conn = mysqli_connect($host, $username, $password, $database) or die("Cannot connect to database!");
 
 //Retrieve list of products
 $sql = "SELECT * FROM Products ORDER BY category DESC";
 
 echo "<div style='background-color: rgb(255,253,250);'>";
+var_dump($_SERVER);
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
