6325e3b93c94b7a02dfb21faed7e929561b4289b
David <ltdatphan@gmail.com>
2020-11-24 21:29:01 -0500

    Added favicon to page

--- templates/header.php
+++ templates/header.php
@@ -22,6 +22,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
         
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="icon" type="image/png" href="../public/assets/img/favicon.png">
         <title><?= $title?></title>
     </head>
     <body>
