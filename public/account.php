ce741262fc0c7696ace9c8592b967011ba8e10ad
CesarEPerez <cesarperezargota@gmail.com>
2020-12-06 18:49:00 -0500

    Edited account page

--- public/account.php
+++ public/account.php
@@ -2,7 +2,6 @@
 $user_results = mysqli_query($conn, "SELECT * FROM Users WHERE email='{$_SESSION['user']['email']}'");
 $user = mysqli_fetch_assoc($user_results);
 
-
 $order_results = mysqli_query($conn, "SELECT * FROM Orders WHERE email='{$_SESSION['user']['email']}'");
 ?>
 <div class="container custom-page">
@@ -10,8 +9,12 @@ $order_results = mysqli_query($conn, "SELECT * FROM Orders WHERE email='{$_SESSI
         <div class="col-5">
             <h2>Account Info</h2>
             <div class="row">
-                <div class="col-4"><b>Full Name:</b></div>
-                <div class="col"><?= "{$_SESSION['user']['fname']} {$_SESSION['user']['lname']}" ?></div>
+                <div class="col-4"><b>First Name:</b></div>
+                <div class="col"><?= "{$_SESSION['user']['fname']}" ?></div>
+            </div>
+            <div class="row">
+                <div class="col-4"><b>Last Name:</b></div>
+                <div class="col"><?= "{$_SESSION['user']['lname']}" ?></div>
             </div>
             <div class="row">
                 <div class="col-4"><b>E-mail:</b></div>
@@ -23,7 +26,7 @@ $order_results = mysqli_query($conn, "SELECT * FROM Orders WHERE email='{$_SESSI
             <?php if (mysqli_num_rows($order_results) === 0) : ?>
                 <b>No orders exist</b>
             <?php else : ?>
-                <table class="table">
+                <table class="table table-striped">
                     <thead>
                         <tr>
                             <th scope="col">Order #</th>
