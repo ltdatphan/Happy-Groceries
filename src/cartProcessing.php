66386766b92167cc3c467f4f7c76202ffe505cd5
Michael <michael.tandoc@ryerson.ca>
2020-12-06 23:33:28 -0500

    Added comments

--- src/cartProcessing.php
+++ src/cartProcessing.php
@@ -9,17 +9,20 @@ function isValidProduct($link, $product_id)
 
 $action = $_GET['action'] ?? null;
 
+// If product ID is invalid, show error.
 if (isset($_POST['id']) && !isValidProduct($conn, (int)$_POST['id'])) {
     echo "<h2>INVALID PRODUCT ID</h2>";
     exit;
 }
 
+// If cart does not exist, initialize it.
 if (!isset($_SESSION['cart']) || !is_array($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
 switch ($action) {
     case 'add':
+        // Add product to cart
         $product_id = (int)$_POST['id'];
         $quantity = (int)$_POST['quantity'];
         if (array_key_exists($product_id, $_SESSION['cart'])) {
@@ -29,18 +32,20 @@ switch ($action) {
         }
         break;
     case 'remove':
-
+        // Remove product from cart
         $product_id = (int)$_POST['id'];
         if (array_key_exists($product_id, $_SESSION['cart'])) {
             unset($_SESSION['cart'][$product_id]);
         }
         break;
     case 'set':
+        // Set product in cart with given quantity
         $product_id = (int)$_POST['id'];
         $quantity = (int)$_POST['quantity'];
         $_SESSION['cart'][$product_id] = $quantity;
         break;
     case 'clear':
+        // Clear cart
         $_SESSION['cart'] = [];
         break;
 }
@@ -53,9 +58,11 @@ $_SESSION['cart'] = array_filter($_SESSION['cart'], function ($var) {
 if (!empty($_SESSION['cart'])) {
     $cart_ids_string = implode(',', $cart_ids = array_keys($_SESSION['cart']));
 
+    // Query products in cart from database
     $result = mysqli_query($conn, "SELECT * FROM Products WHERE id in ($cart_ids_string) ORDER BY prod_name");
 }
 
+// If cart action was performed, redirect to base URL (prevent refreshes causing resubmitting form)
 if (!is_null($action)) {
     header('content-type: text/plain;charset=utf8');
     $url_info = parse_url($_SERVER["REQUEST_URI"]);
--- src/logoutProcessing.php
+++ src/logoutProcessing.php
@@ -1,3 +1,4 @@
 <?php
+// Logout of account by deleting session, and redirect to main page
 session_destroy();
 header("Location: index.php");
