40892aa6e2983711114aad4c0a2ca75eaf696724
Michael <michael.tandoc@ryerson.ca>
2020-12-06 23:19:20 -0500

    Added comments to loginProcessing.php

--- src/loginProcessing.php
+++ src/loginProcessing.php
@@ -7,9 +7,11 @@ if (isset($_POST['login_user'])) {
     $password = mysqli_real_escape_string($conn, trim($_POST['password']));
     $remember = isset($_POST['remember']);
 
+    // Query for users from database with provided email.
     $results = mysqli_query($conn, "SELECT * FROM Users WHERE email='$email'");
     $user = mysqli_fetch_assoc($results);
 
+    // Login user if account exists with provided email and password
     if (!is_null($user) && password_verify($password, $user['password'])) {
         //Login user, and redirect to main page.
         $_SESSION['user'] = ['email' => $email, 'fname' => $user['firstname'], 'lname' => $user['lastname']];
