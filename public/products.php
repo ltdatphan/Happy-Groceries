fd3d7ed03bf4dbd6599b1297632c66815476f498
CesarEPerez <cesarperezargota@gmail.com>
2020-12-06 22:30:04 -0500

    Added comments to products file

--- public/products.php
+++ public/products.php
@@ -3,6 +3,7 @@
         /* Category gotten from the query string */
         $cat_param = $_GET['category'] ?? '';
 
+        /* Used to generate the page header */
         $cat_name = [
             'produce' => 'Produce',
             'meats' => 'Meats',
@@ -12,21 +13,26 @@
             '' => 'All Products'
         ];
 
+        /* Generate page header */
         echo "<h1 class='title-header'>{$cat_name[$cat_param]}</h1>"; 
 
         /*Get the search query*/
         if (isset($_GET['item']))$search_item = $_GET['item'];
         else $search_item = "";
 
+        /* If user searched for an item, display search query here */
         if ($search_item != "") echo "<h4>Showing results for: <em>".$search_item."</em></h4><br>";
 
+        /* Get the products */
         $result = mysqli_query($conn, $sql);
         $num_results = mysqli_num_rows($result);
 
         if ($num_results <= 0) die("Unable to find product data!");
 
+        /* Display the products */
         echo '<div class="row">';
         while($row = mysqli_fetch_assoc($result)) {
+            /* Get all product attributes */
             $category = $row["category"];
             if ($cat_param != "" && $category != $cat_param) continue;
             $prod_name = $row["prod_name"];
@@ -38,17 +44,21 @@
             $source = $row["source"];
             $organic = $row["organic"];
             $prod_url = $row["prod_url"];?>
+            <!-- Generate product card -->
             <div class="col-sm d-flex justify-content-center">
                 <div class="card d-flex products-card justify-content-center shadow p-3 mb-5 rounded">
                     <a href="?page=item&id=<?= $id?>"><img class="card-img-top" src="<?= $prod_url?>" alt="Product image" style="width:100%"></a>
                     <div class="card-body">
+                        <!-- Card body (name, description, price) -->
                         <a href="?page=item&id=<?= $id?>" class="card-text"><h4 class="card-title"><?= $prod_name?></h4></a>
                         <p class="card-text card-description"><?=$prod_desc?></p>
                         <p class="card-text"><?= "\$".sprintf("%.2f",$price)." CAD"?></p>
+                        <!-- Add to cart button -->
                         <form action="?page=products<?=isset($_GET["category"])?'&category='.$_GET['category']:''?>&action=add" method="POST">
                             <div>
                                 <input type="submit" class="btn card-btn" value="Add to cart">
                                 <input type="hidden" name="id" value="<?= $id ?>">
+                        <!-- Quantity -->
                         <div class="quantity-input">
                             <label>Quantity:</label>
                             <input type="number" name="quantity" size="4" value="1" min="1" max="99"/>
